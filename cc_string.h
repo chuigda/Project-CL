f3b991f0b5cc3b9333f73e3ecbcbf48047f63179
ICEY <icey@icey.tech>
2020-09-30 15:30:59 +0800

    Good

--- cc_string.h
+++ cc_string.h
@@ -5,6 +5,10 @@
 extern "C" {
 #endif
 
+void *memchr(const void *str, int c, size_t n);
+
+int memcmp();
+  
 int cc_strcmp(const char *str1, const char *str2);
 
 char *cc_strcpy(char *dest, const char *from);
@@ -14,8 +18,6 @@ void cc_strcat(char *dest, const char *from);
 void cc_strncat(char *dest, const char *from, cc_size_t size);
 
 const char *cc_strstr(const char *str1, const char *str2);
-
-
 
 #ifdef __cplusplus
 } // extern "C"
