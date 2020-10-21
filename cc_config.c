bcbf9d8cab2ce7378079a385d1d5e62ecbd79c49
ICEY <icey@icey.tech>
2020-10-21 18:03:38 +0800

    definition

--- cc_config.c
+++ cc_config.c
@@ -1,4 +1,19 @@
-int main() {
-    /* TODO not implemented yet */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef enum e_prebuilt_config {
+    PBC_ALL_DEFAULT,
+    PBC_ALL_STD,
+    PBC_ALL_CCLIB
+} prebuilt_config;
+
+int main(int argc, const char *argv[]) {
+    if (argc == 2) {
+        if (!strcmp(argv[1], "-h")
+            || !strcmp(argv[1], "--help")) {
+
+        }
+    }
     return 0;
 }
