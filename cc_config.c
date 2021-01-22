a2d742f35749e4d4f81c014d9dc05f3d55751366
Alex Greenall <alex@ckx-ily.cn>
2021-01-22 14:17:01 +0800

    二  局  剧  变

--- cc_config.c
+++ cc_config.c
@@ -2,16 +2,145 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef enum e_prebuilt_config {
-    PBC_ALL_DEFAULT,
-    PBC_ALL_STD,
-    PBC_ALL_CCLIB
-} prebuilt_config;
+typedef struct st_config_item {
+    const char *item_name;
+    const char *item_description;
+    const char *cmd_opt;
+    const char *macro_name;
+    int default_option;
+} config_item;
+
+static config_item items[] = {
+    {
+        "CCLib Allocator",
+        "A special dynamic allocator for low-end embedded devices",
+        "--with-alloc",
+        "CCLIB_ALLOC",
+        0
+    },
+    {
+        "CCLib String Enhancement Library",
+        "Enhancements to string.h, includes many utilities "
+        "especially slices",
+        "--with-strlib",
+        "CCLIB_STRLIB",
+        1
+    },
+    {
+        "CCLib Dynamic Multi-dimension Array",
+        "Dynamic allocated multi dimension array",
+        "--with-dmdarr",
+        "CCLIB_DMDARR",
+        1
+    },
+    {
+        "CCLib Vector Library",
+        "A continuous, O(1) random access container",
+        "--with-vec",
+        "CCLIB_DS_VEC",
+        1
+    },
+    {
+        "CCLib Linked List Library",
+        "A doubly linked list",
+        "--with-llist",
+        "CCLIB_DS_LLIST",
+        1
+    },
+    {
+        "CCLib Intrusive List Library",
+        "An intrusive linked list",
+        "--with-ilist",
+        "CCLIB_DS_ILIST",
+        0
+    },
+    {
+        "CCLib Set Library",
+        "An unordered set container",
+        "--with-set",
+        "CCLIB_DS_SET",
+        1
+    },
+    {
+        "CCLib Map Library",
+        "An unordered map container",
+        "--with-map",
+        "CCLIB_DS_MAP",
+        1
+    },
+    {
+        "CCLib MString Library",
+        "A special arena for managing strings, use handles to represent"
+        " strings, and compare strings by comparing handles.",
+        "--with-mstring",
+        "CCLIB_MSTRING",
+        1
+    },
+    {
+        "CCLib Easy Going Configuration Library",
+        "A minimal structural configuration parser",
+        "--with-egconf",
+        "CCLIB_EGCONF",
+        1
+    },
+    {
+        "CCLib Argument Parser",
+        "A minimal command line argument parser",
+        "--with-argp",
+        "CCLIB_ARGP",
+        1
+    },
+    {
+        "CCLib Test Suite",
+        "A minimal test suite",
+        "--with-cctest",
+        "CCLIB_TEST",
+        0
+    },
+    {
+        "CCLib Tracing Utilities",
+        "A minimal call tracing toolkit",
+        "--with-trace",
+        "CCLIB_TRACE",
+        0
+    }
+};
+
+static const char *help_text = 
+"CCLib Configurator 1.0\n"
+"Iceyey Chuigda WhiteGive <icey@icey.tech>\n"
+"Interactive configurator for CCLib\n"
+"\n"
+"USAGE:\n"
+"    %s [FLAGS]\n"
+"\n"
+"FLAGS:\n"
+"    -d, --default        Use the default configuration\n"
+"    -a, --all            Enable all features\n"
+"    --no-default         Do not enable default features\n"
+"    --with-alloc         Enable dynamic memory allocator\n"
+"    --with-strlib        Enable string enhancement library\n"
+"    --with-dmdarr        Enable dynamic multi-dimension array\n"
+"    --with-vec           Enable vector library\n"
+"    --with-llist         Enable linked list library\n"
+"    --with-ilist         Enable intrusive list library\n"
+"    --with-set           Enable set library\n"
+"    --with-map           Enable map library\n"
+"    --with-mstring       Enable mstring library\n"
+"    --with-egconf        Enable easy going configuration\n"
+"    --with-mdb           Enable minidb library\n"
+"    --with-argp          Enable commandline argument parser library\n"
+"    --with-cctest        Enable CCTest library\n"
+"    --with-trace         Enable tracing library\n";
 
 int main(int argc, const char *argv[]) {
     if (argc == 2) {
         if (!strcmp(argv[1], "-h") || !strcmp(argv[1], "--help")) {
+            const char *prog_name = 
+                (argv[0] && argv[0][0]) ? argv[0] : "cc_config";
+            fprintf(stderr, help_text, prog_name);
         }
     }
     return 0;
 }
+
--- cfg.temp.h
+++ cfg.temp.h
@@ -1,184 +1,47 @@
 #ifndef CCLIB_CFG_TEMP_H
 #define CCLIB_CFG_TEMP_H
 
-/** Feature implemenational choices
- *
- * User may provide implementation on themselves by appointing
- * CC_FEAT_USE_USER. User must provide the interfaces required by
- * header file in their own implementation source file.
- *
- * */
-#define CC_FEAT_DISABLED  /* Disable this feature   */             0
-#define CC_FEAT_USE_STD   /* Wrap C standard library */            1
-#define CC_FEAT_USE_CCLIB /* Use CCLIB's own implementation */     2
-#define CC_FEAT_USE_USER  /* Let user to provide implementation */ 3
+/** Dynamic memory allocation */
+#define CCLIB_ALLOC     1
 
-/** Dynamic memory allocation
- *
- * Possible choices: DISABLED, STD, CCLIB, USER
- *
- *   - If DISABLED choosed, dynamic memory allocation will not be
- *     accessible
- *   - If STD choosed, CCLIB will simply use malloc
- *   - If CCLIB choosed, CCLIB will use an internal, static only
- *     allocator.
- *   - If USER choosed, CCLIB will use implementations from
- *     cc_alloc.user.c
- * */
-#define CCLIB_ALLOC     CC_FEAT_USE_STD
+/** String manipulation library enhancements */
+#define CCLIB_STRLIB    1
 
-/** Integer types library
- *
- * Possible choices: STD, USER
- *
- *   - If STD choosed, CCLIB will use stdint.h and re-export definitions
- *   - If USER choosed, CCLIB will include cc_inttypes.user.h
- *
- * */
-#define CCLIB_INTLIB    CC_FEAT_USE_STD
+/** Dynamic multi-dimension array */
+#define CCLIB_DS_DMARR  1
 
-/** Panic library
- *
- * Possible choices: DISABLED, STD, USER
- *
- *   - If DISABLED choosed, function cc_panic will still be generated,
- *     but with no effect
- *   - If STD choosed, cc_panic will call abort
- *   - If USER choosed, cc_panic will use implementation from
- *     cc_panic.user.c
- *
- * */
-#define CCLIB_PANIC     CC_FEAT_USE_STD
+/** Vector, the continuous storage container */
+#define CCLIB_DS_VEC    1
 
-/** Display library
- *
- * Possible choices: DISABLED, STD, USER
- *
- *   - If DISABLED choosed, display functions will still be generated,
- *     but with no effect
- *   - If STD choosed, display functions will call stdio functions
- *   - If USER choosed, display functions will use implementations
- *     from cc_disp.user.c
- *
- * */
-#define CCLIB_DISP      CC_FEAT_USE_STD
+/** Linked list, the linked container */
+#define CCLIB_DS_LLIST  1
 
-/** Override default signed or unsigned size types of CCLIB.
- *
- * By default, CCLIB uses size_t for unsigned size type, ptrdiff_t
- * for unsigned size type.
- *
- * */
-/* #define CCLIB_SIZE_TYPE size_t */
-/* #define CCLIB_SSIZE_TYPE ptrdiff_t */
+/** Intrusive linked list */
+#define CCLIB_DS_ILIST  1
 
-/** String manipulation library
- *
- * Possible choices: DISABLED, STD, CCLIB, USER
- *
- *   - If STD choosed, CCLIB will use <string.h>
- *   - If CCLIB choosed, CCLIB will use builtin implementation
- *
- * */
-#define CCLIB_STRLIB    CC_FEAT_USE_STD
+/** Set, an unordered collection */
+#define CCLIB_DS_SET    1
 
-/** Dynamic multi-dimension array
- *
- * This feature is inaccessible if CCLIB_ALLOC is DISABLED
- *
- * Possible choices: DISABLED, CCLIB, USER
- *
- * */
-#define CCLIB_DS_DMARR  CC_FEAT_USE_CCLIB
+/** Map, an unordered dictionary */
+#define CCLIB_DS_MAP    1
 
-/** Vector, the continuous storage container
- *
- * This feature is inaccessible if CCLIB_ALLOC is DISABLED
- *
- * Possible choices: DISABLED, CCLIB, USER
- *
- * */
-#define CCLIB_DS_VEC    CC_FEAT_USE_CCLIB
+/** MString, use handle to represent strings */
+#define CCLIB_MSTRING   0
 
-/** Linked list, the linked container
- *
- * This feature is inaccessible if CCLIB_ALLOC is DISABLED
- *
- * Possible choices: DISABLED, CCLIB, GLIB, USER
- *
- * */
-#define CCLIB_DS_LLIST  CC_FEAT_USE_CCLIB
+/** Easy Going Configuration */
+#define CCLIB_EGCONF    1
 
-/** Intrusive linked list
- *
- * Possible choices: DISABLED, CCLIB
- *
- * */
-#define CCLIB_DS_ILIST  CC_FEAT_USE_CCLIB
+/** MiniDB, a simple Key-Value database */
+#define CCLIB_MDB       1
 
-/** Set, an unordered collection
- *
- * This feature is inaccessible if CCLIB_ALLOC is DISABLED
- *
- * Possible choices: DISABLED, CCLIB, USER
- *
- * */
-#define CCLIB_DS_SET    CC_FEAT_USE_CCLIB
+/** ArgParse, command line argument parser */
+#define CCLIB_ARGP      1
 
-/** Map, an unordered dictionary
- *
- * This feature is inaccessible if CCLIB_ALLOC is DISABLED
- *
- * Possible choices: DISABLED, CCLIB, USER
- *
- * */
-#define CCLIB_DS_MAP    CC_FEAT_USE_CCLIB
+/** CCTest, a simple testing library */
+#define CCLIB_TEST      1
 
-/** MString, use handle to represent strings
- *
- * This feature is inaccessible if CCLIB_ALLOC is DISABLED
- *
- * Possible choices: DISABLED, CCLIB
- *
- * */
-#define CCLIB_MSTRING   CC_FEAT_USE_CCLIB
-
-/** Easy Going Configuration, the flat configuration file
- *
- * Possible choices: DISABLED, CCLIB
- *
- * */
-#define CCLIB_EGCONF    CC_FEAT_USE_CCLIB
-
-/** MiniDB, a simple database
- *
- * Possible choices: DISABLED, CCLIB, USER
- *
- * */
-#define CCLIB_MDB       CC_FEAT_USE_CCLIB
-
-/** CCLIB CmdParse, a command line argument parser
- *
- * Possible choices: DISABLED, CCLIB, USER
- *
- * */
-#define CCLIB_CPARSE    CC_FEAT_USE_CCLIB
-
-/** CCLIB Shell, a shell like scripting language
- *
- * This feature is inaccessible if CCLIB_ALLOC is DIABLED
- *
- * Possible choices: DISABLED, CCLIB
- *
- * */
-#define CCLIB_SHELL     CC_FEAT_USE_SHELL
-
-/** CCTest, a simple testing framework
- *
- * Possible choices: DISABLED, CCLIB
- *
- * */
-#define CCLIB_TEST      CC_FEAT_USE_CCLIB
+/** CCTrace, a somewhat useful tracing library */
+#define CCLIB_TRACE     1
 
 #endif /* CCLIB_CFG_TEMP_H */
 
