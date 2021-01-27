f86ffcd994ea081e4ac8002faf69ac8b60c7e834
ICEY <icey@icey.tech>
2021-01-27 18:26:32 +0800

    摸鱼

--- cc_config.c
+++ cc_config.c
@@ -7,6 +7,8 @@ typedef struct st_config_item {
     const char *item_description;
     const char *cmd_opt;
     const char *macro_name;
+    const char *src_file;
+    const char *header_file;
     int enabled;
 } config_item;
 
@@ -16,6 +18,8 @@ static config_item items[] = {
         "A special dynamic allocator for low-end embedded devices",
         "--with-alloc",
         "CCLIB_ALLOC",
+        "cc_alloc.c",
+        "cc_alloc.h",
         0
     },
     {
@@ -24,6 +28,8 @@ static config_item items[] = {
         "especially slices",
         "--with-strlib",
         "CCLIB_STRLIB",
+        "cc_strlib.c",
+        "cc_strlib.h",
         1
     },
     {
@@ -31,6 +37,8 @@ static config_item items[] = {
         "Dynamic allocated multi dimension array",
         "--with-dmdarr",
         "CCLIB_DMDARR",
+        "cc_dmdarr.c", 
+        "cc_dmdarr.h", 
         1
     },
     {
@@ -38,6 +46,8 @@ static config_item items[] = {
         "A continuous, O(1) random access container",
         "--with-vec",
         "CCLIB_DS_VEC",
+        "cc_vec.c", 
+        "cc_vec.h", 
         1
     },
     {
@@ -45,6 +55,8 @@ static config_item items[] = {
         "A doubly linked list",
         "--with-llist",
         "CCLIB_DS_LLIST",
+        "cc_list.c", 
+        "cc_list.h", 
         1
     },
     {
@@ -52,6 +64,8 @@ static config_item items[] = {
         "An intrusive linked list",
         "--with-ilist",
         "CCLIB_DS_ILIST",
+        "cc_ilist.c", 
+        "cc_ilist.h", 
         0
     },
     {
@@ -59,6 +73,8 @@ static config_item items[] = {
         "An unordered set container",
         "--with-set",
         "CCLIB_DS_SET",
+        "cc_set.c", 
+        "cc_set.h", 
         1
     },
     {
@@ -66,6 +82,8 @@ static config_item items[] = {
         "An unordered map container",
         "--with-map",
         "CCLIB_DS_MAP",
+        "cc_map.c", 
+        "cc_map.h", 
         1
     },
     {
@@ -74,6 +92,8 @@ static config_item items[] = {
         " strings, and compare strings by comparing handles.",
         "--with-mstring",
         "CCLIB_MSTRING",
+        "cc_mstring.c", 
+        "cc_mstring.h", 
         1
     },
     {
@@ -81,6 +101,8 @@ static config_item items[] = {
         "A minimal structural configuration parser",
         "--with-egconf",
         "CCLIB_EGCONF",
+        "cc_egconf.c", 
+        "cc_egconf.h", 
         1
     },
     {
@@ -88,6 +110,8 @@ static config_item items[] = {
         "A minimal command line argument parser",
         "--with-argp",
         "CCLIB_ARGP",
+        "cc_argp.c", 
+        "cc_argp.h", 
         1
     },
     {
@@ -95,6 +119,8 @@ static config_item items[] = {
         "A minimal test suite",
         "--with-cctest",
         "CCLIB_TEST",
+        "cc_test.c", 
+        "cc_test.h", 
         0
     },
     {
@@ -102,6 +128,8 @@ static config_item items[] = {
         "A minimal call tracing toolkit",
         "--with-trace",
         "CCLIB_TRACE",
+        "cc_trace.c", 
+        "cc_trace.h", 
         0
     }
 };
@@ -117,6 +145,7 @@ static const char *help_text =
 "    %s [FLAGS]\n"
 "\n"
 "FLAGS:\n"
+"    -i, --interactive    Use interactive configuration mode\n"
 "    -d, --default        Use the default configuration\n"
 "    -a, --all            Enable all features\n"
 "    --no-default         Do not enable default features\n"
@@ -135,17 +164,30 @@ static const char *help_text =
 "    --with-cctest        Enable CCTest library\n"
 "    --with-trace         Enable tracing library\n";
 
+void interactive_config(void) {
+    for (int i = 0; i < COUNTOF(items); i++) {
+        fprintf(stderr, "Would you like to enable %s: %s?\n",
+                items[i].item_name, items[i].item_description);
+    }
+}
+
 int main(int argc, const char *argv[]) {
-    if (argc == 2) {
+    if (argc == 1) {
+        interactive_config();
+    } else if (argc == 2) {
         if (!strcmp(argv[1], "-h") || !strcmp(argv[1], "--help")) {
             const char *prog_name = 
                 (argv[0] && argv[0][0]) ? argv[0] : "cc_config";
             fprintf(stderr, help_text, prog_name);
-        }
-        if (!strcmp(argv[1], "-a") || !strcmp(argv[1], "--all")) {
+            return -1;
+        } else if (!strcmp(argv[1], "-a") || !strcmp(argv[1], "--all")) {
+            fprintf(stderr, "info: enabling all configuration");
             for (size_t i = 0; i < COUNTOF(items); i++) {
                 items[i].enabled = 1;
             }
+        } else if (!strcmp(argv[1], "-d")
+                   || !strcmp(argv[1], "--default")) {
+            fprintf(stderr, "info: using default configuration");
         }
     }
     return 0;
