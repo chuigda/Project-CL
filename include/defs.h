d2cc47a1eae2493f13df677838ad78f4399c17bd
icey <icey@icey.tech>
2023-03-12 04:57:01 +0800

    导出模式

--- include/defs.h
+++ include/defs.h
@@ -38,4 +38,76 @@ typedef PROJECT_CL_UINT8_TYPE cc_uint8;
 typedef uint8_t cc_uint8;
 #endif
 
+#ifdef PROJECT_CL_INT16_TYPE
+typedef PROJECT_CL_INT16_TYPE cc_int16;
+#else
+typedef int16_t cc_int16;
+#endif
+
+#ifdef PROJECT_CL_UINT16_TYPE
+typedef PROJECT_CL_UINT16_TYPE cc_uint16;
+#else
+typedef uint16_t cc_uint16;
+#endif
+
+#ifdef PROJECT_CL_INT32_TYPE
+typedef PROJECT_CL_INT32_TYPE cc_int32;
+#else
+typedef int32_t cc_int32;
+#endif
+
+#ifdef PROJECT_CL_UINT32_TYPE
+typedef PROJECT_CL_UINT32_TYPE cc_uint32;
+#else
+typedef uint32_t cc_uint32;
+#endif
+
+#ifdef PROJECT_CL_USE_INT64
+#   ifdef PROJECT_CL_INT64_TYPE
+typedef PROJECT_CL_INT64_TYPE cc_int64;
+#   else
+typedef int64_t cc_int64;
+#   endif
+#   ifdef PROJECT_CL_UINT64_TYPE
+typedef PROJECT_CL_UINT64_TYPE cc_uint64;
+#   else
+typedef uint64_t cc_uint64;
+#   endif
+#endif
+
+#ifdef PROJECT_CL_USE_INT128
+#   ifdef PROJECT_CL_INT128_TYPE
+typedef PROJECT_CL_INT128_TYPE cc_int128;
+#   else
+#       error PROJECT_CL_INT128_TYPE must be defined when enabling \
+PROJECT_CL_USE_INT128
+#   endif
+#   ifdef PROJECT_CL_UINT128_TYPE
+typedef PROJECT_CL_UINT128_TYPE cc_uint128;
+#   else
+#       error PROJECT_CL_UINT128_TYPE must be defined when enabling \
+PROJECT_CL_USE_UINT128
+#   endif
+#endif
+
+#ifdef PROJECT_CL_FP16_TYPE
+typedef PROJECT_CL_FP16_TYPE cc_f16;
+#endif
+
+#ifdef PROJECT_CL_FP32_TYPE
+typedef PROJECT_CL_FP32_TYPE cc_f32;
+#else
+typedef float cc_f32;
+#endif
+
+#ifdef PROJCT_CL_FP64_TYPE
+typedef PROJECT_CL_FP64_TYPE cc_f64;
+#else
+typedef double cc_f64;
+#endif
+
+#ifdef PROJECT_CL_FP128_TYPE
+typedef PROJECT_CL_FP128_TYPE cc_f128;
+#endif
+
 #endif /* PROJECT_CL_DEFS_H */
--- src/defs.c
+++ src/defs.c
@@ -41,14 +41,31 @@ PROJECT_CL_VALLOC_BUFSIZ value defined
 
 #if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L
 /* https://stackoverflow.com/questions/7469915 */
-#define ISUNSIGNED(T) ((T) -1 > 0)
+#   define ISUNSIGNED(T) ((T) -1 > 0)
 #   define ISSIGNED(T) (!ISUNSIGNED(T))
-_Static_assert(sizeof(cc_int8) == 1,
-               "SANITY CHECK: size of cc_int8 should be 1 byte");
-_Static_assert(ISSIGNED(cc_int8),
-               "SANITY_CHECK: cc_int8 should be signed");
-_Static_assert(sizeof(cc_uint8) == 1,
-               "SANITY CHECK: size of cc_uint8 should be 1 byte");
-_Static_assert(ISUNSIGNED(cc_uint8),
-               "SANITY CHECK: cc_uint8 should be unsigned");
+_Static_assert(ISSIGNED(cc_isize));
+_Static_assert(ISUNSIGNED(cc_size));
+_Static_assert(sizeof(cc_int8) == 1 && ISSIGNED(cc_int8));
+_Static_assert(sizeof(cc_uint8) == 1 && ISUNSIGNED(cc_uint8));
+_Static_assert(sizeof(cc_int16) == 2 && ISSIGNED(cc_int16));
+_Static_assert(sizeof(cc_uint16) == 2 && ISUNSIGNED(cc_uint16));
+_Static_assert(sizeof(cc_int32) == 4 && ISSIGNED(cc_int32));
+_Static_assert(sizeof(cc_uint32) == 4 && ISUNSIGNED(cc_uint32));
+#   ifdef PROJECT_CL_USE_INT64
+_Static_assert(sizeof(cc_int64) == 8 && ISSIGNED(cc_int64));
+_Static_assert(sizeof(cc_uint64) == 8 && ISUNSIGNED(cc_uint64));
+#   endif
+#   ifdef PROJECT_CL_USE_INT128
+_Static_assert(sizeof(cc_int128) == 16 && ISSIGNED(cc_int128));
+_Static_assert(sizeof(cc_uint128) == 16 && ISUNSIGNED(cc_uint128));
+#   endif
+
+#   ifdef PROJECT_CL_FP16_TYPE
+_Static_assert(sizeof(cc_f16) == 2);
+#   endif
+_Static_assert(sizeof(cc_f32) == 4);
+_Static_assert(sizeof(cc_f64) == 8);
+#   ifdef PROJECT_CL_FP128_TYPE
+_Static_assert(sizeof(cc_f128) == 16);
+#   endif
 #endif
