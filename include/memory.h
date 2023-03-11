390cb31ffa62d2a4675f742b87cf428b3e037def
icey <icey@icey.tech>
2023-03-12 05:40:09 +0800

    你有点太极端了.jpg

--- include/memory.h
+++ include/memory.h
@@ -13,6 +13,11 @@ void cc_memcpy(void *dst, void *src, cc_size cnt);
 
 void cc_memmove(void *dst, void *src, cc_size cnt);
 
+#define CC_PTR_OFFSET(ptr, offset) \
+    ((void*)((cc_uint8*)(ptr) + offset))
+#define CC_PTR_OFFSET2(ptr, size, cnt) \
+    ((void*)((cc_uint8*)(ptr) + (size) * (cnt)))
+
 #ifdef __cplusplus
 } /* extern "C" */
 #endif
