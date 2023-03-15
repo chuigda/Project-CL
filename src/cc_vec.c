939aa7b31f9b35aac228fd5896165d194e1cb6bf
Alex Greenall <alex@ckx-ily.cn>
2023-03-15 11:07:45 +0800

    changing vectors

--- src/cc_vec.c
+++ src/cc_vec.c
@@ -116,4 +116,68 @@ void *cc_vec_at(cc_vec *vec, cc_size idx) {
     return CC_PTR_OFFSET2(vec->buf, vec->item_size, idx);
 }
 
+cc_error cc_vec_push(cc_vec *vec, void *value) {
+    RT_CONTRACT_E(vec && value)
+
+    if (!vec->buf) {
+        vec->buf = cc_alloc(vec->item_size);
+        if (!vec->buf) {
+            return CC_OUT_OF_MEMORY;
+        }
+        vec->size = 0;
+        vec->cap = 1;
+    } else if (vec->size == vec->cap) {
+        void *newbuf = cc_alloc(vec->item_size * vec->cap * 2);
+        if (!newbuf) {
+            return CC_OUT_OF_MEMORY;
+        }
+        cc_memcpy(newbuf, vec->buf, vec->size * vec->item_size);
+        cc_free(vec->buf);
+        vec->buf = newbuf;
+        vec->cap *= 2;
+    }
+
+    void *dst = CC_PTR_OFFSET2(vec->buf, vec->item_size, vec->size);
+    cc_memcpy(dst, value, vec->item_size);
+    return CC_NO_ERROR;
+}
+
+cc_error cc_vec_insert(cc_vec *vec, cc_size idx, void *value) {
+    RT_CONTRACT_E(vec && value && idx < vec->size)
+
+    if (!vec->buf) {
+        vec->buf = cc_alloc(vec->item_size);
+        if (!vec->buf) {
+            return CC_OUT_OF_MEMORY;
+        }
+        vec->size = 1;
+        vec->cap = 1;
+        cc_memcpy(vec->buf, value, vec->item_size);
+    } else if (vec->size < vec->cap) {
+        void *dst = CC_PTR_OFFSET2(vec->buf, vec->item_size, idx + 1);
+        void *src = CC_PTR_OFFSET2(vec->buf, vec->item_size, idx);
+        cc_size sz = (vec->size - idx) * vec->item_size;
+        cc_memcpy(dst, src, sz);
+        cc_memcpy(src, value, vec->item_size);
+    } else {
+        void *newbuf = cc_alloc(vec->item_size * vec->cap * 2);
+        if (!newbuf) {
+            return CC_OUT_OF_MEMORY;
+        }
+        cc_memcpy(newbuf, vec->buf, vec->item_size * idx);
+        cc_memcpy(CC_PTR_OFFSET2(newbuf, vec->item_size, idx),
+                  value,
+                  vec->item_size);
+        cc_memcpy(CC_PTR_OFFSET2(newbuf, vec->item_size, idx + 1),
+                  CC_PTR_OFFSET2(vec->buf, vec->item_size, idx),
+                  vec->item_size * (vec->size - idx));
+        cc_free(vec->buf);
+        vec->buf = newbuf;
+        vec->cap *= 2;
+        vec->size += 1;
+    }
+
+    return CC_NO_ERROR;
+}
+
 #endif /* PROJECT_CL_BUILD_VEC */
