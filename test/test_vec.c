b4d72c625822d4f34a3c0a74401a02635a59a289
Alex Greenall <alex@ckx-ily.cn>
2023-03-15 17:01:27 +0800

    changing vektors

--- test/test_vec.c
+++ test/test_vec.c
@@ -1,6 +1,38 @@
 #include "cc_vec.h"
 #include "cc_assert.h"
+#include "cc_abort.h"
 
-int main() {
-    
+#include <stdio.h>
+
+void test1() {
+    cc_vec *v = cc_vec_init(sizeof(int));
+    cc_assert(v);
+
+    int arr[4] = { 114, 514, 1919, 810 };
+    for (cc_size i = 0; i < 4; i++) {
+        cc_vec_push(v, arr + i);
+    }
+
+    cc_assert(cc_vec_size(v) == 4);
+    for (cc_size i = 0; i < 4; i++) {
+        int value = *(int*)cc_vec_at(v, i);
+        cc_assert(value == arr[i]);
+    }
+
+    cc_vec_destroy(v);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        cc_abort();
+    }
+
+    if (!strcmp(argv[2], "1")) {
+        test1();
+    } else {
+        cc_abort();
+    }
+
+    printf("TCAS system test ok\n");
+    return 0;
 }
