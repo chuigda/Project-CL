6f7f6dc47015df11f6617f3e5a975f9bc4c77468
Alex Greenall <alex@ckx-ily.cn>
2023-03-16 16:13:16 +0800

    没事找事，给我夫人泼脏水，红色法拉利，我跟他谈笑风生

---
+++ include/cc_string.h
@@ -0,0 +1,12 @@
+#ifndef PROJECT_CL_STRING_H
+#define PROJECT_CL_STRING_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* PROJECT_CL_STRING_H */
