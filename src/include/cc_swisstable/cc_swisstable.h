4537b60f4fc3728f34958a72239bab96246d1853
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2023-03-25 22:50:12 -0400

    prepare table implementation

--- src/include/cc_swisstable/cc_swisstable.h
+++ src/include/cc_swisstable/cc_swisstable.h
@@ -37,7 +37,7 @@ cc_st_probe_seq cc_st_probe_seq_create(cc_size position, cc_size stride) {
 }
 
 static inline
-void cc_st_probe_seq_move_next(cc_st_probe_seq * seq, cc_size bucket_mask) {
+void cc_st_probe_seq_move_next(cc_st_probe_seq *seq, cc_size bucket_mask) {
     seq->stride += sizeof(cc_st_group);
     seq->position += seq->stride;
     seq->position &= bucket_mask;
@@ -71,5 +71,53 @@ cc_size cc_st_bucket_mask_to_capacity(cc_size bucket_mask) {
 static const cc_st_ctrl CC_ST_CONST_EMPTY_GROUP[128] = {
         CC_ST_DUPLICATE_128(CC_ST_EMPTY)
 };
+
+static inline
+void cc_st_table_layout(
+        cc_size elem_size,
+        cc_size elem_alignment,
+        cc_size num_of_buckets,
+        cc_size *offset,
+        cc_size *size
+) {
+    *offset = (num_of_buckets * elem_size + elem_alignment - 1)
+              & ~(elem_alignment - 1);
+    *size = *offset + num_of_buckets + sizeof(cc_st_group);
+}
+
+typedef struct {
+    cc_st_ctrl *ptr;
+    cc_size element_size;
+} cc_st_bucket;
+
+static inline
+cc_st_bucket cc_st_bucket_create(cc_st_ctrl *base, cc_size index, cc_size element_size) {
+    cc_st_bucket bucket;
+    bucket.ptr = base - index * element_size;
+    bucket.element_size = element_size;
+    return bucket;
+}
+
+static inline
+cc_st_bucket cc_st_bucket_move(cc_st_bucket from, cc_size offset) {
+    cc_st_bucket bucket;
+    bucket.ptr = from.ptr - offset * from.element_size;
+    bucket.element_size = from.element_size;
+    return bucket;
+}
+
+static inline
+void *cc_st_bucket_get(cc_st_bucket bucket) {
+    return bucket.ptr - bucket.element_size;
+}
+
+typedef struct {
+    cc_size bucket_mask;
+    cc_size element_size;
+    cc_size element_alignment;
+    cc_st_ctrl *ctrl;
+    cc_size growth_left;
+    cc_size items;
+} cc_swisstable;
 
 #endif // PROJECT_CL_SWISSTABLE_H
