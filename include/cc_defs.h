c46eda4a510c24680250841735e5505f1dd923f4
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2023-04-01 10:48:12 -0400

    undo attribute change

--- include/cc_defs.h
+++ include/cc_defs.h
@@ -218,14 +218,6 @@ _Bool cc_opaque_predicate(_Bool x) {
 #   endif
 #endif
 
-#ifndef CC_ATTRIBUTE_COLD
-#   if defined(__has_attribute) && __has_attribute(cold)
-#       define CC_ATTRIBUTE_COLD __attribute__((cold))
-#   else
-#       define CC_ATTRIBUTE_COLD
-#   endif
-#endif
-
 typedef struct {
     cc_size size;
     cc_size alignment;
