47c4f3067f9d583d53749543e6b8dfce81486790
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2023-04-01 10:24:48 -0400

    implemement a simple assert

--- include/cc_assert.h
+++ include/cc_assert.h
@@ -3,17 +3,24 @@
 
 #include <cc_defs.h>
 
+#if defined(PROJECT_CL_ASSERT_USE_LIBC) && defined(PROJECT_CL_ASSERT_USE_CCLIB)
+#   error "multiple implementation of assert is not allowed"
+#endif
+
 #if !defined(PROJECT_CL_BUILD_ASSERT) || defined(NDEBUG)
 #   define cc_assert(x)
 #elif defined(PROJECT_CL_ASSERT_USE_LIBC)
 #   include <assert.h>
 #   define cc_assert assert
 #else
-#   define cc_assert(x) cc_assert_impl((x), ##x, __FILE__, __LINE__);
-extern "C" void cc_assert_impl(int value,
-                               const char *expr,
-                               const char *file,
-                               int line);
+#   define cc_assert(x) cc_assert_impl((x), #x, __FILE__, __LINE__);
+#ifdef __cplusplus
+extern "C"
+#endif
+void cc_assert_impl(int value,
+                    const char *expr,
+                    const char *file,
+                    int line);
 #endif
 
 #endif /* PROJECT_CL_CC_ASSERT_H */
--- src/cc_assert.c
+++ src/cc_assert.c
@@ -1,7 +1,51 @@
 #include <cc_assert.h>
-
+#include <cc_abort.h>
+// TODO: use our own strlen
+#include <string.h>
+#if defined(__linux__) || defined(__APPLE__)
+#   include <unistd.h>
+#endif
 #if defined(PROJECT_CL_BUILD_ASSERT) && \
     !defined(NDEBUG) && \
     !defined(PROJECT_CL_ASSERT_USE_LIBC)
-/* TODO implement cc_assert_impl, would require other things */
+void cc_assert_impl(int value,
+                    const char *expr,
+                    const char *file,
+                    int line) {
+  // use opaque predicate anyway.
+  // can potentially help in LTO.
+  if (!cc_opaque_predicate(value)) {
+#if defined(__linux__) || defined(__APPLE__)
+    if (fsync(STDERR_FILENO)) {}
+    if (fsync(STDOUT_FILENO)) {}
+    char begin[] = "\n\nassertion failure: \n\t- expr: ";
+    char mid[] = "\n\t- position: ";
+    char line_str[64];
+    int line_size = 0;
+    if (line <= 0) {
+        line_str[line_size++] = '0';
+    } else {
+        int base = 1000000000;
+        while (base) {
+          if (line >= base) {
+            line_str[line_size++] = (char)((line / base) + '0');
+            line %= base;
+          }
+          base /= 10;
+        }
+    }
+    line_str[line_size] = 0;
+    if (write(STDERR_FILENO, begin, strlen(begin))) {}
+    if (write(STDERR_FILENO, expr, strlen(expr))) {}
+    if (write(STDERR_FILENO, mid, strlen(mid))) {}
+    if (write(STDERR_FILENO, file, strlen(file))) {}
+    if (write(STDERR_FILENO, ":", 1)) {}
+    if (write(STDERR_FILENO, line_str, line_size)) {}
+    if (write(STDERR_FILENO, "\n", 1)) {}
+    if (fsync(STDERR_FILENO)) {}
+#endif
+    cc_dump_stacktrace();
+    cc_abort();
+  }
+}
 #endif
