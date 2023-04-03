ce4214e64cc59abb5b1932bcaadebdde712d1c20
Rebuild <admin@rebuild.moe>
2023-04-02 23:21:12 -0400

    encoding docs

--- include/cc_encoding.h
+++ include/cc_encoding.h
@@ -12,15 +12,23 @@ extern "C" {
 #endif
 
 /**
-* base64_encode - Base64 encode
-* @src: Data to be encoded
-* @len: Length of the data to be encoded
-* @out_len: Pointer to output length variable, or %NULL if not used
-* Returns: Allocated buffer of out_len bytes of encoded data,
-* or empty string on failure
-*/
+ * @brief Base64 encode
+ * @src: Data to be encoded
+ * @len: Length of the data to be encoded
+ * @o_len: Pointer to output length variable, or @c NULL if not used
+ * @returns Allocated string of o_len + 1 bytes of encoded data,
+ * or @c NULL on failure
+ */
 cc_char *cc_base64_encode(const void *src, const cc_size len, cc_size *o_len);
 
+/**
+ * @brief Base64 decode
+ * @data: Data to be decoded
+ * @len: Length of the data to be decoded
+ * @o_len: Pointer to output length variable, or @c NULL if not used
+ * @returns Allocated buffer of decoded data,
+ * or @c NULL on failure
+ */
 cc_uchar *cc_base64_decode(const cc_char *data, const cc_size len, cc_size *o_len);
 
 #ifdef __cplusplus
